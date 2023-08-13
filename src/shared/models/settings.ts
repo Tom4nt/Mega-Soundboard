4edfdea85ea7e1e95b9be0e939617d9859059df8
Tomás Antunes <tmsntns@gmail.com>
2023-08-13 12:58:23 +0100

    added migration from the old settings format

--- src/shared/models/settings.ts
+++ src/shared/models/settings.ts
@@ -1,4 +1,5 @@
 import { Optional } from "../interfaces";
+import Keys from "../keys";
 import { ActionName, actions } from "../quickActions";
 
 export type OptionalSettings = Optional<Settings>;
@@ -7,11 +8,6 @@ export type OptionalSettings = Optional<Settings>;
 export default class Settings {
     constructor(
         public minToTray = true,
-        // public stopSoundsKeys: number[] = [],
-        // public enableKeybinds = true,
-        // public enableKeybindsKeys: number[] = [],
-        // public overlapSounds = true,
-        // public loopSounds = false,
         public mainDevice = "default",
         public secondaryDevice = "",
         public mainDeviceVolume = 100,
@@ -20,8 +16,6 @@ export default class Settings {
         public latestLogViewed = 0,
         public soundsLocation = "",
         public pttKeys: number[] = [],
-        // public randomSoundKeys: number[] = [],
-        // public loopSoundsKeys: number[] = [],
         public processKeysOnRelease = false,
         public windowSize: number[] = [],
         public windowPosition: number[] = [],
@@ -49,15 +43,54 @@ export default class Settings {
     }
 }
 
-// TODO: Convert old static properties to new dynamic Quick Settings.
 export function convertSettings(data: { [key: string]: unknown }): Settings {
     const settings = new Settings();
+
     for (const iterator of Object.keys(new Settings()) as (keyof Settings)[]) {
         const val = data[iterator];
         const defaultType = typeof settings[iterator];
-        if (typeof val === defaultType && (Array.isArray(settings[iterator]) == Array.isArray(val))) {
+        if (typeof val === defaultType && (Array.isArray(settings[iterator]) === Array.isArray(val))) {
             settings[iterator] = val as never;
         }
     }
+
+    if (Object.keys(settings.quickActionStates).length === 0)
+        settings.quickActionStates = migrateQuickSettingsStates(data);
+
+    if (Object.keys(settings.quickActionKeys).length === 0)
+        settings.quickActionKeys = migrateQuickSettingsKeys(data);
+
     return settings;
+}
+
+function migrateQuickSettingsStates(data: { [key: string]: unknown }): { [name: string]: boolean } {
+    const res: { [name: string]: boolean } = {};
+
+    const keys = ["enableKeybinds", "overlapSounds"];
+    const actionNames: ActionName[] = ["toggleKeybinds", "toggleSoundOverlap"];
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const actionName = actionNames[i];
+        if (typeof data[key] === "boolean")
+            res[actionName] = data[key] as boolean;
+    }
+
+    return res;
+}
+
+function migrateQuickSettingsKeys(data: { [key: string]: unknown }): { [name: string]: number[] } {
+    const res: { [name: string]: number[] } = {};
+
+    const keys = ["enableKeybindsKeys", "stopSoundsKeys", "randomSoundKeys"];
+    const actionNames: ActionName[] = ["toggleKeybinds", "stopSounds", "playRandomSound"];
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const actionName = actionNames[i];
+        if (Keys.isKeys(data[key]))
+            res[actionName] = data[key] as number[];
+    }
+
+    return res;
 }
